872c12d5744bb76a8b277ab3d6dfb46fcb42614b
0xMazout <10938862+0xMazout@users.noreply.github.com>
2023-11-29 21:30:18 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import Head from 'next/head';
+import Link from 'next/link';
+import * as React from 'react';
+
+/**
+ * SVGR Support
+ * Caveat: No React Props Type.
+ *
+ * You can override the next-env if the type is important to you
+ * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
+ */
+import Logo from '~/svg/Logo.svg';
+
+// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
+// Before you begin editing, follow all comments with `STARTERCONF`,
+// to customize the default configuration.
+
+export default function HomePage() {
+  return (
+    <main>
+      <Head>
+        <title>Hi</title>
+      </Head>
+      <section className='bg-white'>
+        <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
+          <Logo className='w-16' />
+          <h1 className='mt-4'>Next.js + Tailwind CSS + TypeScript Starter</h1>
+          <p className='mt-2 text-sm text-gray-800'>
+            A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
+            Import, Seo, Link component, pre-configured with Husky{' '}
+          </p>
+          <p className='mt-2 text-sm text-gray-700'>
+            <Link href='https://github.com/0xMazout/ts-nextjs-tailwind-starter-fullpack-approuter'>
+              See the repository
+            </Link>
+          </p>
+
+          <Link
+            href='https://vercel.com/new/git/external?repository-url=https://github.com/0xMazout/ts-nextjs-tailwind-starter-fullpack-approuter'
+            className='mt-4'
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              width='92'
+              height='32'
+              src='https://vercel.com/button'
+              alt='Deploy with Vercel'
+            />
+          </Link>
+
+          <footer className='absolute bottom-2 text-gray-700'>
+            © {new Date().getFullYear()} By{' '}
+            <Link href='https://github.com/0xMazout/ts-nextjs-tailwind-starter-fullpack-approuter'>
+              0xMazout
+            </Link>
+          </footer>
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ src/constant/config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  url: 'http://localhost:3000',
+  title: 'Next.js + Tailwind CSS + TypeScript Starter',
+  description:
+    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+};
